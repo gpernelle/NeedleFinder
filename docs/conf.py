ff889129e286b9cadba79e52d4c73ef7457923cb
Guillaume Pernelle <gpernelle@gmail.com>
2014-12-02 12:27:47 +0000

    relative path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,8 +39,8 @@ for mod_name in MOCK_MODULES:
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('/Users/guillaume/Projects/github/NeedleFinderAmastm/NeedleFinder/NeedleFinder'))
-
+# sys.path.insert(0, os.path.abspath('/Users/guillaume/Projects/github/NeedleFinderAmastm/NeedleFinder/NeedleFinder'))
+sys.path.append(os.path.abspath('../NeedleFinder'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
